1060a09b6cb086dd6e77a5ab58381b561f3409f8
sashahino14 <sashaurielhino@gmail.com>
2025-12-27 23:57:45 +0000

    Ajout des pseudos

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'hilink-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/socket.io/socket.io.js'
+];
+
+// Installation
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activation (nettoyage)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// Interception des requêtes (Mode hors ligne basique)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
